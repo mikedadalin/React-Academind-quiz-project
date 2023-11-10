c2a5837e7bc6a4e1f46df0a5791e726a439dcf79
Mike Lin <mikedadalin@gmail.com>
2023-11-10 00:50:47 +0000

    shuffle the answer options and display the quiz complete img

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,16 +1,30 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  const handleSelectorAnswer = (selectAnswer) => {
+  const handleSelectorAnswer = (selectedAnswer) => {
     setUserAnswers((prevState) => {
-      return [...preState, selectedAnswer];
+      return [...prevState, selectedAnswer];
     });
   };
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
 
   return (
     <div id="quiz">
