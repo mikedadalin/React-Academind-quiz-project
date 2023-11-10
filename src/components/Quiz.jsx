873a3a0d7f12af04920cec5210d5f3811803aa7d
Mike Lin <mikedadalin@gmail.com>
2023-11-10 01:45:49 +0000

    add the question timer

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import QUESTIONS from "../questions.js";
 import quizCompleteImg from "../assets/quiz-complete.png";
+import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -8,11 +9,16 @@ export default function Quiz() {
   const activeQuestionIndex = userAnswers.length;
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  const handleSelectorAnswer = (selectedAnswer) => {
+  const handleSelectorAnswer = useCallback((selectedAnswer) => {
     setUserAnswers((prevState) => {
       return [...prevState, selectedAnswer];
     });
-  };
+  }, []);
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectorAnswer(null),
+    [handleSelectorAnswer],
+  );
 
   if (quizIsComplete) {
     return (
@@ -29,6 +35,11 @@ export default function Quiz() {
   return (
     <div id="quiz">
       <div id="question">
+        <QuestionTimer
+          key={activeQuestionIndex}
+          timeout={10000}
+          onTimeout={handleSkipAnswer}
+        />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {QUESTIONS[activeQuestionIndex].answers.map((answer) => {
